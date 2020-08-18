095933501cc3db12b8cef7379d2cccb942f6aa42
Nesom <rakhimovsamatarrr@gmail.com>
2020-08-18 11:34:23 +0300

    added house

---
+++ New Unity Project/Assets/Scripts/Building.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public Vector2Int Size = Vector2Int.one;
+
+
+    private void OnDrawGizmosSelected()
+    {
+        for (int x = 0; x < Size.x; x++)
+        {
+            for (int y = 0; y< Size.y; y++)
+            { 
+                Gizmos.color = Color.red;
+                Gizmos.DrawCube(transform.position + new Vector3(x,0,y), new Vector3(1, .1f, 1) * transform.localScale.x);
+            }
+        }
+    }
+}
