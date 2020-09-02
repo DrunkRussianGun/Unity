c2136b09ec60f442767512f5a406feb8add5e6ab
azaliyas-github <69823031+azaliyas-github@users.noreply.github.com>
2020-09-02 22:29:29 +0300

    Сделан спаунер кабана и управление клонами через клавиши

---
+++ New Unity Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform SpawnPosition;
+    public GameObject SpawnObject; 
+    public float TimeForNewSpawn;
+
+    void Start()
+    {
+        StartCoroutine(SpawnCD());
+    }
+
+    void Repeat()
+    {
+        StartCoroutine(SpawnCD());
+    }
+    
+    IEnumerator SpawnCD()
+    {
+    	yield return new WaitForSeconds(TimeForNewSpawn);
+    	Instantiate(SpawnObject, SpawnPosition.position, Quaternion.identity);
+    	Repeat();
+    }
+}
