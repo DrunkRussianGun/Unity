156876c3413cc9d9f103279818905a0003708c1f
azaliyas-github <69823031+azaliyas-github@users.noreply.github.com>
2021-02-06 22:36:43 +0300

    Написала инструмент для логирования идентификаторов выделяемых сущностей

---
+++ New Unity Project/Assets/Tools/SelectedInstanceIdLogger.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Tools
+{
+	[ExecuteInEditMode]
+	public class SelectedInstanceIdLogger : MonoBehaviour
+	{
+		#region Singleton
+
+		private static volatile bool isInitialized;
+
+		private static readonly object synchronizationLock = new object(); 
+
+		#endregion
+
+		public void OnEnable()
+		{
+			if (isInitialized)
+				return;
+			
+			lock (synchronizationLock)
+			{
+				if (isInitialized)
+					return;
+				isInitialized = true;
+
+				var oldCallback = Selection.selectionChanged;
+				Selection.selectionChanged = () =>
+				{
+					oldCallback?.Invoke();
+					OnSelectionChange();
+				};
+			}
+		}
+
+		private void OnSelectionChange()
+		{
+			if (Selection.instanceIDs.Length != 1)
+				return;
+
+			var selectedObject = Selection.activeObject;
+			if (!selectedObject)
+				return;
+
+			Debug.Log($"Выбран объект {selectedObject.name} с ID {selectedObject.GetInstanceID()}");
+		}
+	}
+}
